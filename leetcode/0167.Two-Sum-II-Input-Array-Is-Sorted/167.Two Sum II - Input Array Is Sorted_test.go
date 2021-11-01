5e8d9bbde49c16044ac007a4080eba45cc7dda27
otfot <dsfat.lover@gmail.com>
2021-11-01 22:21:21 +0800

    test: add 0167 test

---
+++ leetcode/0167.Two-Sum-II-Input-Array-Is-Sorted/167.Two Sum II - Input Array Is Sorted_test.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSumII(t *testing.T) {
+	type param struct {
+		numbers []int
+		target  int
+	}
+
+	type answer struct {
+		one []int
+	}
+
+	data := []struct {
+		param
+		answer
+	}{
+		{
+			param{[]int{2, 7, 11, 15}, 9},
+			answer{[]int{1, 2}},
+		},
+		{
+			param{[]int{2, 3, 7, 7, 9}, 16},
+			answer{[]int{3, 5}},
+		},
+		{
+			param{[]int{2, 6, 9, 12, 14, 18}, 3},
+			answer{[]int{}},
+		},
+	}
+
+	for _, pa := range data {
+		actual := twoSum(pa.param.numbers, pa.param.target)
+
+		if actual == nil && len(pa.answer.one) == 0 {
+			continue
+		} else if !reflect.DeepEqual(pa.answer.one, actual) {
+			t.Errorf("twoSum(%v, %v) = %v; expected %v", pa.param.numbers, pa.param.target, actual, pa.answer.one)
+		}
+	}
+}
