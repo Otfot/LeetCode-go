8b79193e505976994db2c96b29c3be2e19ca489e
otfot <dsfat.lover@gmail.com>
2021-11-01 22:17:56 +0800

    style: delete 0001 useless comment

--- leetcode/0001.Two-Sum/1.Two Sum.go
+++ leetcode/0001.Two-Sum/1.Two Sum.go
@@ -1,14 +1,13 @@
 package leetcode
 
-
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int, len(nums))
 	for k, v := range nums {
 		if idx, ok := m[target-v]; ok {
 			return []int{idx, k}
 		}
-		m[v] = k 
+		m[v] = k
 	}
-	// if possible, should return nil 
+
 	return nil
 }
