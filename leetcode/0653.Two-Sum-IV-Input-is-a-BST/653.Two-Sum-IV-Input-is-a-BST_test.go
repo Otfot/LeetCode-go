53ba94f292f9c2772955910aced855ef3560040d
otfot <dsfat.lover@gmail.com>
2021-11-08 23:08:24 +0800

    test: add 0653 test

---
+++ leetcode/0653.Two-Sum-IV-Input-is-a-BST/653.Two-Sum-IV-Input-is-a-BST_test.go
@@ -0,0 +1,53 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+	. "github.com/otfot/LeetCode-go/tools"
+)
+
+func TestTwoSum(t *testing.T) {
+	type param struct {
+		root []int
+		k int
+	}
+
+	type answer struct {
+		one bool
+	}
+
+	data := []struct {
+		param
+		answer
+	}{
+		{
+			param{[]int{}, 3},
+			answer{false},
+		},
+		{
+			param{[]int{3, 9, 20, NULL, NULL, 15, 7}, 29},
+			answer{true},
+		},
+
+		{
+			param{[]int{1, 2, 3, 4, NULL, NULL, 5}, 9},
+			answer{true},
+		},
+
+		{
+			param{[]int{1, 2, 3, 4, NULL, 5}, 4},
+			answer{true},
+		},
+
+		
+	}
+
+	for _, pa := range data {
+		actual := findTarget(Ints2NewTreeNode(pa.param.root), pa.param.k)
+
+		if !reflect.DeepEqual(actual, pa.answer.one) {
+			t.Errorf("findTarget(%v, %v) = %v; expected %v", Ints2NewTreeNode(pa.param.root), pa.param.k, actual, pa.answer.one)
+		}
+	}
+
+}
